7c3362f261b5139c859c7bd3fbd3f43889ce4889
Celeste Winterton <celestewinterton@gmail.com>
2022-03-08 13:00:17 -0800

    Static Assets

---
+++ serve-static-assets/server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const indexFile = fs.readFileSync('./index.html', 'utf-8');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(indexFile);
+});
+
+const port = 5002;
+
+server.listen(port, () => console.log('Server is listening on port', port));
